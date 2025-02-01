89898e3095b8d28709e1522b8c64d44f450e0706
Mario Vilches <110475661+mfvilches@users.noreply.github.com>
2025-02-01 01:25:12 +0000

    clase

--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,9 @@ def db_connect():
     engine = create_engine(os.getenv('DATABASE_URL'))
     engine.connect()
     return engine
+
+def transformar_columnas_object(df):
+    for column in df.columns:
+        if df[column].dtype == 'object':
+            df[column+'_n'] = pd.factorize(df[column])[0]
+    return df
